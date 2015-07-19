e3504526c80ae8def51ea8fa5466f899843c013e
monocasual <monocasual@gmail.com>
2015-07-19 10:43:04 +0200

    Add support for nested directories in source browser

--- apps/frontend/controllers/SourceController.php
+++ apps/frontend/controllers/SourceController.php
@@ -7,12 +7,17 @@ namespace Giada\Frontend\Controllers;
 class SourceController extends BaseController
 {
 
-	private $token = 'a97c99cd5b97f6d4675f84e5c72efbd94b909eff:x-oauth-basic';
 	private $jsonService;
+	private $token = 'a97c99cd5b97f6d4675f84e5c72efbd94b909eff:x-oauth-basic';
+	private $blacklist = array(
+		'src/vst',
+		'src/rtaudio-mod'
+	);
 
 	/* ------------------------------------------------------------------------ */
 
-	private function getCommits($file) {
+	private function getCommits($file)
+	{
 		$this->jsonService->init('https://api.github.com/repos/monocasual/giada/commits?path='.$file, $this->token);
 		$data = $this->jsonService->get(true);
 		return $data;
@@ -20,8 +25,9 @@ class SourceController extends BaseController
 
 	/* ------------------------------------------------------------------------ */
 
-	private function getSource($file) {
-		$this->jsonService->init('https://api.github.com/repos/monocasual/giada/contents/'.$file, $this->token);
+	private function getNode($n)
+	{
+		$this->jsonService->init('https://api.github.com/repos/monocasual/giada/contents/'.$n, $this->token);
 		$data = $this->jsonService->get(true);
 		if (!array_key_exists('message', $data))
 			return $data;
@@ -30,12 +36,52 @@ class SourceController extends BaseController
 
 	/* ------------------------------------------------------------------------ */
 
-	private function getDir($dir) {
-		$this->jsonService->init('https://api.github.com/repos/monocasual/giada/contents/'.$dir, $this->token);
-		$data = $this->jsonService->get(true);
-		if (!array_key_exists('message', $data))
-			return $data;
-		return 0;
+	private function isFile($node)
+	{
+		// Keys of the array
+    $keys = array_keys($node);
+
+    // If the array keys of the keys match the keys, then the array must
+    // not be associative (e.g. the keys array looked like {0:0, 1:1...}).
+    return array_keys($keys) !== $keys;
+	}
+
+	/* ------------------------------------------------------------------------ */
+
+	private function showFile($file)
+	{
+		$cacheKey = 'source-'.str_replace(array('.', '/'), '-', $file);
+
+		if (!$this->view->getCache()->exists($cacheKey))
+		{
+			$this->view->setVar('file', $file);
+			$source = $this->getNode($file);
+			if ($source == 0)
+				$this->response->redirect('404');
+			$this->view->setVar('type', 'file');
+			$this->view->setVar('file', $file);
+			$this->view->setVar('size', number_format($source['size']));
+			$this->view->setVar('html_url', $source['html_url']);
+			$this->view->setVar('content', htmlentities(base64_decode($source['content'])));
+			$this->view->setVar('commits', $this->getCommits($file));
+		}
+		$this->view->cache(array('key' => $cacheKey));
+	}
+
+	/* ------------------------------------------------------------------------ */
+
+	private function showDir($d)
+	{
+		$cacheKey = 'source-'.str_replace(array('.', '/'), '-', $d);
+		if (!$this->view->getCache()->exists($cacheKey))
+			$source = $this->getNode($d);
+			if ($source == 0)
+				$this->response->redirect('404');
+			$this->view->setVar('type', 'dir');
+			$this->view->setVar('blacklist', $this->blacklist);
+			$this->view->setVar('path', $d);
+			$this->view->setVar('dir', $source);
+		$this->view->cache(array('key' => $cacheKey));
 	}
 
 	/* ------------------------------------------------------------------------ */
@@ -49,32 +95,19 @@ class SourceController extends BaseController
 
 	public function indexAction()
 	{
-		$cacheKey = 'source-index';
-		if (!$this->view->getCache()->exists($cacheKey))
-			$this->view->dir = $this->getDir('src');
-		$this->view->cache(array('key' => $cacheKey));
+		$this->showDir('src');
 	}
 
 	/* ------------------------------------------------------------------------ */
 
 	public function showAction($file)
 	{
-		/* avoid dots in cache filename */
-		$cacheKey = 'source-show-'.str_replace('.', '-', $file);
-
-		if (!$this->view->getCache()->exists($cacheKey))
-		{
-			$this->view->setVar('file', $file);
-			$source = $this->getSource('src/'.$file);
-			if ($source == 0)
-				$this->response->redirect('404');
-			$this->view->file     = $file;
-			$this->view->size     = number_format($source['size']);
-			$this->view->html_url = $source['html_url'];
-			$this->view->content  = htmlentities(base64_decode($source['content']));
-			$this->view->commits  = $this->getCommits('src/'.$file);
-		}
-		$this->view->cache(array('key' => $cacheKey));
+		$path = implode('/', $this->dispatcher->getParams());
+		$node = $this->getNode($path);
+		if ($this->isFile($node))
+			$this->showFile($path);
+		else
+			$this->showDir($path);
 	}
 }
 
