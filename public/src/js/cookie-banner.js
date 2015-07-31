0bb9150a928a63f463a3ee73fced241861a91bf8
monocasual <monocasual@gmail.com>
2015-07-31 10:07:39 +0200

    Hide cookie banner by default

--- public/src/js/cookie-banner.js
+++ public/src/js/cookie-banner.js
@@ -1,6 +1,9 @@
 if (typeof $.cookie.get('cookie-policy') !== 'undefined') {
   $('.cookie-banner').hide();
 }
+else {
+  $('.cookie-banner').show();
+}
 
 $('.cookie-banner__close').click(function() {
   $.cookie.set('cookie-policy', true, { expires: 365 }); // one year
