a90183fc9d30f6ae042500929e17b0af1242d71c
monocasual <monocasual@gmail.com>
2015-12-24 19:30:56 +0100

    Add methods for fetching social data

--- apps/frontend/controllers/IndexController.php
+++ apps/frontend/controllers/IndexController.php
@@ -6,9 +6,73 @@ namespace Giada\Frontend\Controllers;
 
 class IndexController extends BaseController
 {
-	public function initialize()
+	private function countFacebookShares()
 	{
+		$this->json->init('http://graph.facebook.com/?id=' . $this->config->urls->base);
+		$res = $this->json->get(true);
+		if (array_key_exists('shares', $res)) {
+			return (int) $res['shares'];
+		}
+		else {
+			$this->logger->error('[IndexController::countFacebookShares] invalid API data!');
+			return 0;
+		}
 	}
+
+
+	private function countRedditShares()
+	{
+		$this->json->init('http://www.reddit.com/api/info.json?url=' . $this->config->urls->base);
+		$res = $this->json->get(true);
+		if (array_key_exists('data', $res)) {
+			$score = 0;
+			foreach($res['data']['children'] as $child)
+				$score += (int) $child['data']['score'];
+			return $score;
+		}
+		else {
+			$this->logger->error('[IndexController::countRedditShares] invalid API data!');
+			return 0;
+		}
+	}
+
+
+	private function countFlattrShares()
+	{
+		$this->json->init('https://api.flattr.com/rest/v2/things/' . $this->config->services->flattr_id . '/flattrs');
+		$res = $this->json->get(true);
+		if (is_array($res)) {  // APIs return an array
+			return (int) $res[0]['thing']['flattrs'];
+		}
+		else {
+			$this->logger->error('[IndexController::countFlattrShares] invalid API data!');
+			return 0;
+		}
+	}
+
+
+	private function countGithubShares()
+	{
+		/* sort by number of stars, desc, so Giada App will be always the first
+		 * (unless giada-www or giada-midimaps will beat it, which is unlikely) */
+
+		$this->json->init('https://api.github.com/search/repositories?q=giada+user:monocasual&sort=stars&order=desc');
+		$res = $this->json->get(true);
+		if (array_key_exists('items', $res)) {
+			return (int) $res['items'][0]['stargazers_count'];
+		}
+		else {
+			$this->logger->error('[IndexController::countGithubShares] invalid API data!');
+			return 0;
+		}
+	}
+
+
+	public function onConstruct()
+	{
+		$this->json = \Phalcon\DI::getDefault()->getShared('jsonService');
+	}
+
 
 	public function indexAction()
 	{
@@ -19,7 +83,15 @@ class IndexController extends BaseController
 				'order' => 'date DESC',
 				'limit' => 4
 			));
-			$this->view->news = $news;
+
+			$socialStats = array();
+			$socialStats['facebook'] = $this->countFacebookShares();
+			$socialStats['reddit'] = $this->countRedditShares();
+			$socialStats['flattr'] = $this->countFlattrShares();
+			$socialStats['github'] = $this->countGithubShares();
+
+			$this->view->setVar('news', $news);
+			$this->view->setVar('socialStats', $socialStats);
 		}
 		$this->view->cache(array('key' => $cacheKey));
 	}
