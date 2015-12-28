fd4d0ddde930836c3ffaaff30ddc866eb6dc7ebb
monocasual <monocasual@gmail.com>
2015-12-28 22:52:21 +0100

    Add .ready wrapper

--- public/src/js/cookie-banner.js
+++ public/src/js/cookie-banner.js
@@ -1,11 +1,13 @@
-if (typeof $.cookie.get('cookie-policy') !== 'undefined') {
-  $('.cookie-banner').hide();
-}
-else {
-  $('.cookie-banner').show();
-}
+$(document).ready(function() {
+  if (typeof $.cookie.get('cookie-policy') !== 'undefined') {
+    $('.cookie-banner').hide();
+  }
+  else {
+    $('.cookie-banner').show();
+  }
 
-$('.cookie-banner__close').click(function() {
-  $.cookie.set('cookie-policy', true, { expires: 365 }); // one year
-  $('.cookie-banner').hide();
+  $('.cookie-banner__close').click(function() {
+    $.cookie.set('cookie-policy', true, { expires: 365 }); // one year
+    $('.cookie-banner').hide();
+  });
 });
