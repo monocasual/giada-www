d34e3664595fc304b83beaf00b915d4c6f90c9b5
monocasual <monocasual@gmail.com>
2016-03-06 15:07:32 +0100

    Refactor Download controller

--- apps/frontend/controllers/DownloadController.php
+++ apps/frontend/controllers/DownloadController.php
@@ -6,81 +6,27 @@ namespace Giada\Frontend\Controllers;
 
 class DownloadController extends BaseController
 {
-	public function indexAction()
+	private function updateStats($os)
 	{
-		$this->view->md5win32 = md5_file('../data/dist/'.'giada-'.$this->config->changelog->version.'-win-i386.zip');
-		$this->view->md5lin32 = md5_file('../data/dist/'.'giada-'.$this->config->changelog->version.'-lin-i386.tar.gz');
-		$this->view->md5lin64 = md5_file('../data/dist/'.'giada-'.$this->config->changelog->version.'-lin-amd64.tar.gz');
-		$this->view->md5osx32 = md5_file('../data/dist/'.'giada-'.$this->config->changelog->version.'-osx-i386.zip');
-		$this->view->md5src   = md5_file('../data/dist/'.'giada-'.$this->config->changelog->version.'-src.tar.gz');
+		$stats = \Giada\Models\Stats::findFirst('version="'.$os.'"');
+		$stats->increment();
+		if (!$stats->save())
+		{
+			$this->logger->error('[DownloadController::grabAction] unable to increment stats!');
+			foreach ($stats->getMessages() as $m)
+				$this->logger->error('[DownloadController::grabAction] ... '.$m);
+		}
 	}
 
-	public function grabAction($os, $version=NULL)
+	private function getPath($platform)
 	{
-		$this->view->disable();
+		$base = '../data/dist';
+		$version = $this->config->changelog->version;
+		return sprintf('%s/giada-%s-%s', $base, $version, $platform);
+	}
 
-		/* if 'version' != NULL an old version is being requested */
-
-		if (!$version)
-			$version = $this->config->changelog->version;
-
-		$filepath = '';
-
-		/* input parsing */
-
-		if ($os == 'windows-32')
-			$filepath = 'giada-'.$version.'-win-i386.zip';
-		else
-		if ($os == 'linux-32')
-			$filepath = 'giada-'.$version.'-lin-i386.tar.gz';
-		else
-		if ($os == 'linux-64')
-			$filepath = 'giada-'.$version.'-lin-amd64.tar.gz';
-		else
-		if ($os == 'osx-32')
-			$filepath = 'giada-'.$version.'-osx-i386.zip';
-		else
-		if ($os == 'source')
-			$filepath = 'giada-'.$version.'-src.tar.gz';
-		else
-		if ($os == 'linux-binary-32')
-			$filepath = 'giada-'.$version.'-linbin-i386.tar.gz';
-		else
-		if ($os == 'linux-binary-64')
-			$filepath = 'giada-'.$version.'-linbin-amd64.tar.gz';
-		else
-		if ($os == 'windows-old')
-			$filepath = 'giada-'.$version.'-win.zip';
-		else
-		if ($os == 'osx-old')
-			$filepath = 'giada-'.$version.'-osx.zip';
-		else
-		if ($os == 'linux-binary-old')
-			$filepath = 'giada-'.$version.'-linbin.tar.gz';
-		else
-		if ($os == 'linux-old')
-			$filepath = 'giada-'.$version.'-lin.tar.gz';
-		else
-		{
-			$this->logger->error('[DownloadController::grabAction] wrong version chosen! was $filepath='.$filepath.', $os='.$os);
-			$this->response->redirect('404');
-		}
-
-		/* update download stats, only if not in debug mode */
-
-		if (!$this->config->mode->debug)
-		{
-			$stats = \Giada\Models\Stats::findFirst('version="'.$os.'"');
-			$stats->increment();
-			if (!$stats->save())
-			{
-				$this->logger->error('[DownloadController::grabAction] unable to increment stats!');
-				foreach ($stats->getMessages() as $m)
-					$this->logger->error('[DownloadController::grabAction] ... '.$m);
-			}
-		}
-
-		$filepath = '../data/dist/' . $filepath;
+	private function flush($filepath)
+	{
 		if (!file_exists($filepath))
 			$this->logger->error('[DownloadController::grabAction] file "'.$filepath.'" not found!');
 		if (!is_readable($filepath))
@@ -93,6 +39,49 @@ class DownloadController extends BaseController
 		$this->response->setHeader('Content-Disposition', 'attachment; filename="'.basename($filepath).'"');
 		$this->response->setHeader('Content-Length', filesize($filepath));
 		readfile($filepath);
+	}
+
+	public function indexAction()
+	{
+		$this->view->md5win32 = md5_file($this->getPath('win-i386.zip'));
+		$this->view->md5lin32 = md5_file($this->getPath('lin-i386.tar.gz'));
+		$this->view->md5lin64 = md5_file($this->getPath('lin-amd64.tar.gz'));
+		$this->view->md5osx32 = md5_file($this->getPath('osx-i386.zip'));
+		$this->view->md5src   = md5_file($this->getPath('src.tar.gz'));
+	}
+
+	public function grabAction($os, $version=NULL)
+	{
+		$this->view->disable();
+
+		$filepath = '';
+
+		switch($os)
+		{
+			case 'windows-32':
+				$filepath = $this->getPath('win-i386.zip');
+				break;
+			case 'linux-32':
+				$filepath = $this->getPath('lin-i386.tar.gz');
+				break;
+			case 'linux-64':
+				$filepath = $this->getPath('lin-amd64.tar.gz');
+				break;
+			case 'osx-64':
+				$filepath = $this->getPath('osx-amd64.zip');
+				break;
+			case 'source':
+				$filepath = $this->getPath('src.tar.gz');
+				break;
+			default:
+				$this->logger->error('[DownloadController::grabAction] wrong version chosen! was $os='.$os);
+				$this->response->redirect('404');
+		}
+
+		if (!$this->config->mode->debug)
+			$this->updateStats($os);
+
+		$this->flush($filepath);
 	}
 }
 
